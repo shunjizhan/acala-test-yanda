81bfe650174f306486eee64467e51c62a63827cc
Shunji Zhan <shunhahaha@live.com>
2022-04-25 20:26:38 +0800

    fix mandala deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,36 @@
 const { ethers, upgrades } = require("hardhat");
 const { txParams } = require("../utils/transactionHelpers.js");
+const { EvmRpcProvider } = require("@acala-network/eth-providers");
+
 
 async function main() {
   const ethParams = await txParams();
 
-  const [deployer] = await ethers.getSigners();
+  /* -------------------------------------------------------------------------------------- */
+  // https://github.com/OpenZeppelin/openzeppelin-upgrades/issues/85#issuecomment-1028435049
 
-  const MyToken = await ethers.getContractFactory("MyToken");
-  const instance = await upgrades.deployProxy(MyToken, {
+  // Wrap the provider so we can override fee data.
+  const provider = EvmRpcProvider.from('ws://localhost:9944');
+  await provider.isReady();
+
+  provider.getFeeData = async () => ({
     gasPrice: ethParams.txGasPrice,
     gasLimit: ethParams.txGasLimit,
   });
 
+  // Create the signer for the mnemonic, connected to the provider with hardcoded fee data
+  const signer = ethers.Wallet.fromMnemonic('fox sight canyon orphan hotel grow hedgehog build bless august weather swarm').connect(provider);
+
+  /* -------------------------------------------------------------------------------------- */
+
+  const MyToken = await ethers.getContractFactory("MyToken", signer);
+  const instance = await upgrades.deployProxy(MyToken);
+
   await instance.deployed();
 
   console.log("MyToken deployed at:", instance.address);
+
+  await provider.disconnect();
 }
 
 main()
--- test/MyToken.js
+++ test/MyToken.js
@@ -1,6 +1,7 @@
+// const ethers = require("ethers");
 const { ethers, upgrades } = require("hardhat");
 const { expect } = require("chai");
-const { calcEthereumTransactionParams } = require("@acala-network/eth-providers");
+const { calcEthereumTransactionParams, calcSubstrateTransactionParams, EvmRpcProvider } = require("@acala-network/eth-providers");
 
 const txFeePerGas = '199999946752';
 const storageByteDeposit = '100000000000000';
@@ -17,15 +18,31 @@ describe("MyToken contract", function () {
             storageByteDeposit
         });
 
-        const MyToken = await ethers.getContractFactory("MyToken");
-        
-        const instance = await upgrades.deployProxy(MyToken, {
+        /* -------------------------------------------------------------------------------------- */
+        // https://github.com/OpenZeppelin/openzeppelin-upgrades/issues/85#issuecomment-1028435049
+
+        // Wrap the provider so we can override fee data.
+        const provider = EvmRpcProvider.from('ws://localhost:9944');
+        await provider.isReady();
+
+        provider.getFeeData = async () => ({
             gasPrice: ethParams.txGasPrice,
             gasLimit: ethParams.txGasLimit,
         });
 
+        // Create the signer for the mnemonic, connected to the provider with hardcoded fee data
+        const signer = ethers.Wallet.fromMnemonic('fox sight canyon orphan hotel grow hedgehog build bless august weather swarm').connect(provider);
+
+        /* -------------------------------------------------------------------------------------- */
+
+        const MyToken = await ethers.getContractFactory("MyToken", signer);
+        
+        const instance = await upgrades.deployProxy(MyToken);
+
         const value = await instance.totalSupply();
 
         expect(ethers.utils.formatEther(value)).to.equal('1000000.0');
+
+        await provider.disconnect();
     });
 });
